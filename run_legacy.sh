4757ba38d1ea8c3da0a1993471053abb00cc202f
mavogel <mavogel@posteo.de>
2016-10-20 20:53:04 +0200

    Script aborts now if any command fails. Added check if docker is running.

--- run_legacy.sh
+++ run_legacy.sh
@@ -1,25 +1,42 @@
 #!/bin/sh
+
+###################
+# Config
+###################
+# Aborts the script if a command fails
+set -e
+
 ###################
 # Vars and helpers
 ###################
-SP='********'
+SP='********';
 function info () {
-  echo "\n${SP}${SP}${SP}${SP}${SP}${SP}${SP}${SP}${SP}${SP}${SP}${SP}";
+  echo "";
+  echo "${SP}${SP}${SP}${SP}${SP}${SP}${SP}${SP}${SP}${SP}${SP}${SP}";
   echo "$SP [SCRIPT INFO] $1";
-  echo "${SP}${SP}${SP}${SP}${SP}${SP}${SP}${SP}${SP}${SP}${SP}${SP}\n";
+  echo "${SP}${SP}${SP}${SP}${SP}${SP}${SP}${SP}${SP}${SP}${SP}${SP}";
+  echo "";
 }
 
 ###################
 ## Go woop woop
 ###################
 # 1: Check for required deps in the $PATH
+info "Check for needed binaries in PATH";
 BINS_TO_CHECK=( mvn docker docker-compose )
 for BIN in ${BINS_TO_CHECK[@]}; do command -v $BIN >/dev/null 2>&1 || { echo >&2 "I require '$BIN' but it's not installed.  Aborting."; exit 1; }; done
-info "Needed binaries found in path";
+echo "-> Fine √"
+
+# 1.1 check if docker deamon runs
+info "Checking if docker is running";
+docker ps
+echo "-> Fine √"
 
 # 2: Build legacy webshop??
 info "Bulding legacy web shop"
-cd LegacyWebShop && mvn clean package && cd ..
+cd LegacyWebShop
+mvn clean package
+cd ..
 
 # 3: Build initialized MySQL Database image
 info "Building initialized MySQL Database image"
