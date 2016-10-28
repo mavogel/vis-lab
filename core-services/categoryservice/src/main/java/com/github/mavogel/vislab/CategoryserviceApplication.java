3ec33c5a51c0667c3734f6795059f25035b2ac78
mavogel <mavogel@posteo.de>
2016-10-28 19:16:05 +0200

    Added Skeleton for core curd repo aka category service. Adaped ignores for core and composite services.

---
+++ core-services/categoryservice/src/main/java/com/github/mavogel/vislab/CategoryserviceApplication.java
@@ -0,0 +1,12 @@
+package com.github.mavogel.vislab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategoryserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CategoryserviceApplication.class, args);
+	}
+}
