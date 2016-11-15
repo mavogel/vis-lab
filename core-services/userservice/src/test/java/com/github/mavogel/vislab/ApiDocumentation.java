9d6f806e8a84474f76dc3e3925718b02a5940bba
mavogel <mavogel@posteo.de>
2016-11-15 13:27:16 +0100

    [ci skip] Draft for user service.

---
+++ core-services/userservice/src/test/java/com/github/mavogel/vislab/ApiDocumentation.java
@@ -0,0 +1,153 @@
+package com.github.mavogel.vislab;
+/*
+ *  The MIT License (MIT)
+ *
+ *  Copyright (c) 2016 Manuel Vogel
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ *
+ *  https://opensource.org/licenses/MIT
+ */
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.mavogel.vislab.user.repository.UserRepository;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.restdocs.JUnitRestDocumentation;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+
+/**
+ * Created by mavogel on 11/1/16.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiDocumentation {
+
+    @Rule
+    public JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation("target/generated-snippets");
+
+    private MockMvc mockMvc;
+
+    private RestDocumentationResultHandler documentationHandler;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    private UserRepository categoryRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Before
+    public void setUp() {
+        this.documentationHandler = document("{method-name}",
+                preprocessResponse(prettyPrint()));
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context)
+                .apply(documentationConfiguration(this.restDocumentation))
+                .alwaysDo(this.documentationHandler)
+                .build();
+    }
+
+    @Test
+    public void listUsers() throws Exception {
+
+    }
+
+//    @Test
+//    public void listCategories() throws Exception {
+//        createSampleCategory(1L, "TestCategory");
+//
+//        this.mockMvc.perform(get("/categories").accept(MediaType.APPLICATION_JSON)
+//                .header("Authorization: Basic", "0b79bab50daca910b000d4f1a2b675d604257e42"))
+//                .andExpect(status().isOk())
+//                .andDo(this.documentationHandler.document(
+//                        responseFields(
+//                                fieldWithPath("[].id").description("The category ID"),
+//                                fieldWithPath("[].name").description("The name of the category")
+//                        )
+//                ));
+//    }
+//
+//    @Test
+//    public void addCategory() throws Exception {
+//        Map<String, String> newCategory = new HashMap<>();
+//        newCategory.put("id", "1");
+//        newCategory.put("name", "TestCategory");
+//
+//        ConstrainedFields fields = new ConstrainedFields(Category.class);
+//
+//        this.mockMvc.perform(post("/categories")
+//                .header("Authorization: Basic", "0b79bab50daca910b000d4f1a2b675d604257e42")
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .content(this.objectMapper.writeValueAsString(newCategory)))
+//                .andExpect(status().isCreated())
+//                .andDo(this.documentationHandler.document(
+//                        requestFields(
+//                                fields.withPath("id").description("The category ID"),
+//                                fields.withPath("name").description("The name of the category")
+//                        )
+//                ));
+//    }
+//
+//    @Test
+//    public void deleteCategory() throws Exception {
+//        Category originalCategory = createSampleCategory(1L, "TestCategory");
+//
+//        this.mockMvc.perform(delete("/categories/" + originalCategory.getId())
+//                .header("Authorization: Basic", "0b79bab50daca910b000d4f1a2b675d604257e42")
+//                .accept(MediaType.APPLICATION_JSON))
+//                .andExpect(status().isNoContent());
+//    }
+//
+//    /**
+//     * HELPERS
+//     */
+//    private static class ConstrainedFields {
+//
+//        private final ConstraintDescriptions constraintDescriptions;
+//
+//        ConstrainedFields(Class<?> input) {
+//            this.constraintDescriptions = new ConstraintDescriptions(input);
+//        }
+//
+//        private FieldDescriptor withPath(String path) {
+//            return fieldWithPath(path).attributes(key("constraints").value(StringUtils
+//                    .collectionToDelimitedString(this.constraintDescriptions
+//                            .descriptionsForProperty(path), ". ")));
+//        }
+//    }
+//
+//    private Category createSampleCategory(final Long id, final String name) {
+//        return categoryRepository.save(new Category(id, name));
+//    }
+}
