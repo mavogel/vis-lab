181b228becb844258036abb9bacd2f9a4ec9ddbe
mavogel <mavogel@posteo.de>
2016-11-17 08:38:20 +0100

    Documented add/registerUser.

--- core-services/userservice/src/test/java/com/github/mavogel/vislab/ApiDocumentation.java
+++ core-services/userservice/src/test/java/com/github/mavogel/vislab/ApiDocumentation.java
@@ -26,6 +26,9 @@ package com.github.mavogel.vislab;
  */
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.mavogel.vislab.user.model.Role;
+import com.github.mavogel.vislab.user.model.User;
+import com.github.mavogel.vislab.user.repository.RoleRepository;
 import com.github.mavogel.vislab.user.repository.UserRepository;
 import org.junit.Before;
 import org.junit.Rule;
@@ -35,19 +38,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.JUnitRestDocumentation;
+import org.springframework.restdocs.constraints.ConstraintDescriptions;
 import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+import org.springframework.restdocs.payload.FieldDescriptor;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.StringUtils;
 import org.springframework.web.context.WebApplicationContext;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.snippet.Attributes.key;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
@@ -68,7 +80,10 @@ public class ApiDocumentation {
     private WebApplicationContext context;
 
     @Autowired
-    private UserRepository categoryRepository;
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
 
     @Autowired
     private ObjectMapper objectMapper;
@@ -121,42 +136,40 @@ public class ApiDocumentation {
                 ));
     }
 
-//    @Test
-//    public void listCategories() throws Exception {
-//        createSampleCategory(1L, "TestCategory");
-//
-//        this.mockMvc.perform(get("/categories").accept(MediaType.APPLICATION_JSON)
-//                .header("Authorization: Basic", "0b79bab50daca910b000d4f1a2b675d604257e42"))
-//                .andExpect(status().isOk())
-//                .andDo(this.documentationHandler.document(
-//                        responseFields(
-//                                fieldWithPath("[].id").description("The category ID"),
-//                                fieldWithPath("[].name").description("The name of the category")
-//                        )
-//                ));
-//    }
-//
-//    @Test
-//    public void addCategory() throws Exception {
-//        Map<String, String> newCategory = new HashMap<>();
-//        newCategory.put("id", "1");
-//        newCategory.put("name", "TestCategory");
-//
-//        ConstrainedFields fields = new ConstrainedFields(Category.class);
-//
-//        this.mockMvc.perform(post("/categories")
-//                .header("Authorization: Basic", "0b79bab50daca910b000d4f1a2b675d604257e42")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(this.objectMapper.writeValueAsString(newCategory)))
-//                .andExpect(status().isCreated())
-//                .andDo(this.documentationHandler.document(
-//                        requestFields(
-//                                fields.withPath("id").description("The category ID"),
-//                                fields.withPath("name").description("The name of the category")
-//                        )
-//                ));
-//    }
-//
+    @Test
+    public void registerUser() throws Exception {
+        final Role initRole = new Role("roletype", 1);
+        roleRepository.save(initRole);
+
+        Map<String, Object> newUser = new HashMap<>();
+        newUser.put("id", "1");
+        newUser.put("username", "jdoe");
+        newUser.put("firstname", "John");
+        newUser.put("lastname", "Doe");
+        newUser.put("password", "sdkfjld%3§");
+        Map<String, String> newRole = new HashMap<>();
+        newRole.put("id", "1");
+        newRole.put("type", "roletype");
+        newRole.put("level", "1");
+        newUser.put("role", newRole);
+
+        this.mockMvc.perform(post("/user")
+                .header("Authorization: Basic", "0b79bab50daca910b000d4f1a2b675d604257e42")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(this.objectMapper.writeValueAsString(newUser)))
+                .andExpect(status().isCreated())
+                .andDo(this.documentationHandler.document(
+                        requestFields(
+                                fieldWithPath("id").description("The user ID"),
+                                fieldWithPath("username").description("The name of the user"),
+                                fieldWithPath("firstname").description("The firstname of the user"),
+                                fieldWithPath("lastname").description("The lastname of the user"),
+                                fieldWithPath("password").description("The password of the user"),
+                                fieldWithPath("role").description("The role of the user")
+                        )
+                ));
+    }
+
 //    @Test
 //    public void deleteCategory() throws Exception {
 //        Category originalCategory = createSampleCategory(1L, "TestCategory");
@@ -167,23 +180,6 @@ public class ApiDocumentation {
 //                .andExpect(status().isNoContent());
 //    }
 //
-//    /**
-//     * HELPERS
-//     */
-//    private static class ConstrainedFields {
-//
-//        private final ConstraintDescriptions constraintDescriptions;
-//
-//        ConstrainedFields(Class<?> input) {
-//            this.constraintDescriptions = new ConstraintDescriptions(input);
-//        }
-//
-//        private FieldDescriptor withPath(String path) {
-//            return fieldWithPath(path).attributes(key("constraints").value(StringUtils
-//                    .collectionToDelimitedString(this.constraintDescriptions
-//                            .descriptionsForProperty(path), ". ")));
-//        }
-//    }
 //
 //    private Category createSampleCategory(final Long id, final String name) {
 //        return categoryRepository.save(new Category(id, name));
