04a030619e35d65a904edebaf2ff534b04d004d3
mavogel <mavogel@posteo.de>
2016-11-25 13:23:27 +0100

    Unified parent pom. Added Discovery client, ribbon and hystrix annotations. Still need to test in docker env.

--- composite-services/modifyservice/src/main/java/com/github/mavogel/vislab/ModifyServiceApplication.java
+++ composite-services/modifyservice/src/main/java/com/github/mavogel/vislab/ModifyServiceApplication.java
@@ -28,8 +28,16 @@ package com.github.mavogel.vislab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableCircuitBreaker
+@EnableHystrix //TODO
+@RibbonClient("modifyservice")
 public class ModifyServiceApplication {
 
     public static void main(String[] args) {
--- core-services/userservice/src/main/java/com/github/mavogel/vislab/UserserviceApplication.java
+++ core-services/userservice/src/main/java/com/github/mavogel/vislab/UserserviceApplication.java
@@ -28,8 +28,16 @@ package com.github.mavogel.vislab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableCircuitBreaker
+@EnableHystrix //TODO
+@RibbonClient("userservice")
 public class UserserviceApplication {
 
     public static void main(String[] args) {
