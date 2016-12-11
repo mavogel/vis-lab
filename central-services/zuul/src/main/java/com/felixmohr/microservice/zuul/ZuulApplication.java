360757228a4ad2340e8c34aad003edc33eef59b8
mavogel <mavogel@posteo.de>
2016-12-11 14:23:21 +0100

    Removed Dockerfiles.

--- central-services/zuul/src/main/java/com/felixmohr/microservice/zuul/ZuulApplication.java
+++ central-services/zuul/src/main/java/com/felixmohr/microservice/zuul/ZuulApplication.java
@@ -3,11 +3,11 @@ package com.felixmohr.microservice.zuul;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.sidecar.EnableSidecar;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-@EnableZuulProxy
+@EnableSidecar
 public class ZuulApplication {
 
 	public static void main(String[] args) {
