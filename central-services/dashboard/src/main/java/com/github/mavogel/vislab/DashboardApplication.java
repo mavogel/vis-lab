ac9ecd0e7a608498899782a340650dd371cf87ad
mavogel <mavogel@posteo.de>
2016-12-20 18:28:00 +0100

    Fixed ports for core services and dashboard. Adaped compose file as well.

--- central-services/dashboard/src/main/java/com/github/mavogel/vislab/DashboardApplication.java
+++ central-services/dashboard/src/main/java/com/github/mavogel/vislab/DashboardApplication.java
@@ -3,9 +3,11 @@ package com.github.mavogel.vislab;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableHystrixDashboard
 public class DashboardApplication {
 
     public static void main(String[] args) {
