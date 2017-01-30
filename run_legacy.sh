7fa383110aea9cc60d00821e959c62bcaab99fc7
mavogel <mavogel@posteo.de>
2017-01-30 21:29:18 +0100

    Refined docker image build for legacy webshop. Simplified build process.

--- run_legacy.sh
+++ run_legacy.sh
@@ -2,19 +2,14 @@
 
 source run_base_script.sh
 
-info "Check if ENV variables were set"
-[ -z "$MYSQL_WEBSHOP_DB_ADDR" ] && echo "Need to set MYSQL_WEBSHOP_DB_ADDR ENV-var. Run '$ source export_vars.sh' first!" && exit 1;
-[ -z "$MYSQL_WEBSHOP_DB_DATABASE" ] && echo "Need to set MYSQL_WEBSHOP_DB_DATABASE ENV-var. Run '$ source export_vars.sh' first!" && exit 1;
-[ -z "$MYSQL_WEBSHOP_DB_USER" ] && echo "Need to set MYSQL_WEBSHOP_DB_USER ENV-var. Run '$ source export_vars.sh' first!" && exit 1;
-[ -z "$MYSQL_WEBSHOP_DB_PASSWORD" ] && echo "Need to set MYSQL_WEBSHOP_DB_PASSWORD ENV-var. Run '$ source export_vars.sh' first!" && exit 1;
-
-# 2: Build legacy webshop??
+# 2: Build legacy webshop
 info "Bulding legacy web shop"
 cd LegacyWebShop && mvn clean package && cd ..
+docker build -t mavogel/vis-web-shop-db-image ./LegacyWebShop
 
 # 3: Build initialized MySQL Database image
 info "Building initialized MySQL Database image"
-docker build -t web-shop-db-image -f ./LegacyWebShop/DockerfileMySQL ./LegacyWebShop
+docker build -t mavogel/vis-web-shop-db-image -f ./LegacyWebShop/DockerfileMySQL ./LegacyWebShop
 
 # 4: Compose all together and init db
 info "Composing MySQL Server container, tomcat8 and deploying webshop war"
@@ -22,5 +17,4 @@ docker-compose -f docker-compose-legacy.yml up -d --remove-orphans
 
 # 5: done
 info "Legacy WebShop started -> 'http://localhost:8888/EShop-1.0.0/'"
-
-docker-compose -f docker-compose-legacy.yml logs -tf
+info "get logs via: 'docker-compose -f docker-compose-legacy.yml logs -tf'"
